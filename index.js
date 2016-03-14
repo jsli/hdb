6b9f2ca5a35eb35766b626138625063f03e6fa31
jsli <manson.li3307@gmail.com>
2016-03-14 10:54:50 +0800

    refactor install function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var shell = require('shelljs');
+var installer = require('lib/install');
 
 const HAIBU_PATH_ON_DEVICE = '/data/local/haibu/apps/';
 
@@ -30,60 +31,16 @@ function HDB() {
   }
 }
 
-HDB.prototype.run = function(options) {
+HDB.prototype.install = function(options) {
   // console.log(JSON.stringify(options));
   switch (options.type) {
     case 'local':
-      new LocalRunnable(this).run(options);
+      new installer.LocalRunnable(this).run(options);
       break;
     case 'git':
-      new GitRunnable(this).run(options);
+      new installer.GitRunnable(this).run(options);
       break;
     case 'zip':
       break;
   }
 };
-
-function LocalRunnable(hdb) {
-  this.hdb = hdb;
-}
-
-LocalRunnable.prototype.run = function(options) {
-  var cmd = 'adb push ' + options.location + ' ' +
-    HAIBU_PATH_ON_DEVICE + options.name;
-  if (shell.exec(cmd).code !== 0) {
-    console.error('Error: Push app failed');
-    shell.exit(1);
-  } else {
-    console.log('Push app successfully!');
-  }
-};
-
-function GitRunnable(hdb) {
-  this.hdb = hdb;
-}
-
-GitRunnable.prototype.run = function(options) {
-  var appName = parseAppNameFromGitUrl(options.location);
-  var cmd = 'git clone ' + options.location + ' ' + this.hdb.pkgDir + appName;
-  shell.exec('rm -rf ' + this.hdb.pkgDir + appName);
-  if (shell.exec(cmd).code !== 0) {
-    console.error('Error: Git clone failed');
-    shell.exit(1);
-  } else {
-    new LocalRunnable().run({
-      type: 'local',
-      location: this.hdb.pkgDir + appName,
-      name: appName
-    });
-  }
-};
-
-/*
- * eg: https://github.com/Marak/hellonode.git
- *     'hellonode' should be returned
- */
-function parseAppNameFromGitUrl(url) {
-  var suffix = url.substr(url.lastIndexOf('/') + 1);
-  return suffix.substr(0, suffix.lastIndexOf('.'));
-}
